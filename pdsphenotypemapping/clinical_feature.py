9dbe7fbc83323766167e0721b4fa80258a37868e
Hao Xu <xuh@cs.unc.edu>
2020-05-02 17:13:26 -0400

    read all files in dir

--- pdsphenotypemapping/clinical_feature.py
+++ pdsphenotypemapping/clinical_feature.py
@@ -229,6 +229,7 @@ def age(patient, unit, timestamp):
         if "birthDate" in patient:
             birth_date = patient["birthDate"]
             date_of_birth = strtodate(birth_date)
+            today = timestamp.strftime("%Y-%m-%d")
             mage = calculate_age2(date_of_birth, timestamp)
             return mage.map(lambda age: {
                 "variableValue": {
