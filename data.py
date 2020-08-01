616e0f6085c64931f6ad0ceb320211ac733597d6
xu-hao <xuh@cs.unc.edu>
2020-08-01 03:16:19 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,27 +1,8 @@
 def select(dictionary, fields):
     return {field : dictionary[field] for field in fields}
 
-def get(d, k):
-    return d[k]
-
-def sub(a, b):
-    return a - b
-
-def add(a, b):
-    return a + b
-
-def eq(a,b):
-    return a == b
-
-def ne(a,b):
-    return a != b
-
 def coalesce(value, value2):
     return value2 if value is None else value
-
-def contains(coll, elem):
-    return elem in coll
-
 
 
 
