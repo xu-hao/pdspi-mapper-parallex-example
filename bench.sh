6b4016068295dc67a33a4c59bd40a09d43c90206
Hao Xu <xuh@cs.unc.edu>
2020-08-13 10:03:38 -0400

    update bench to match api

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
-time python cli.py spec8.py spec8 ../data.yaml 1 1 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
-time python cli.py spec8.py spec8 ../data.yaml 16 0 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
-time python cli.py spec8.py spec8 ../data.yaml 16 1 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
+time python cli.py spec8.py spec8 1 1 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
+time python cli.py spec8.py spec8 16 0 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
+time python cli.py spec8.py spec8 16 1 ../resourceTypes.yaml ../patientIds.yaml 2020-01-01T00:00:00Z 8080 8081
