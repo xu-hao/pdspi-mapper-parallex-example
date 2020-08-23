854b6ca47f769c466005e39bb59170121acf5cb0
Hao Xu <xuh@cs.unc.edu>
2020-08-23 14:07:47 -0400

    provide default config

---
+++ config/config.py
@@ -0,0 +1,21 @@
+return {
+    "title": "tx-parallex variable mapper",
+    "pluginType": "m",
+    "pluginTypeTitle": "Mapping",
+    "pluginSelectors": [],
+    "supportedPatientVariables": [
+        {
+            "id": "bmi_before",
+            "title": "bmi before",
+            "legalValues": {"type": "number"}
+        }, {
+            "id": "bmi_after",
+            "title": "bmi after",
+            "legalValues": {"type": "numberboolean"}
+        }, {
+            "id": "outcome",
+            "title": "outcome",
+            "legalValues": {"type": "boolean"}
+        }
+    ]
+}
