c2fed856052bb3b56940004447f3fe62806de9d5
Hao Xu <xuh@cs.unc.edu>
2020-08-22 22:29:06 -0400

    retrieve config

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from tx.parallex import start_python
 from tx.functional.maybe import Just, Nothing
+from tx.functional.utils import identity
 from pathvalidate import validate_filename
 import yappi
 import json
@@ -11,6 +12,8 @@ yappi.set_clock_type(os.environ.get("CLOCK_TYPE", "wall"))
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+config_url = os.environ.get("CONFIG_URL")
 
 def assign(array, keys, value):
     if len(keys) == 0:
@@ -40,36 +43,34 @@ def assign(array, keys, value):
         
         return array
 
+    
+def getModelParameter(modelParameters, modelParameterId, proc, default):
+    specNames = [modelParameter for modelParameter in modelParameters if modelParameter["id"] == modelParameterId]
+    if len(specNames) == 0:
+        specName = default()
+    else:
+        specName = proc(specNames[0]["parameterValue"]["value"])
+    return specName
+
 
 def mappingClinicalFromData(body):
+    config = get_config()
     settingsRequested = body.get("settingsRequested", {})
+    settingsDefault = config.get("settingsDefault", {})
     modelParameters = settingsRequested.get("modelParameters", [])
-    patientVariables = settingsRequested.get("patientVariables", [])
-    logger.info(f"modelParameters = {modelParameters}")
-    specNames = [modelParameter for modelParameter in modelParameters if modelParameter["id"] == "specName"]
-    logger.info(f"specNames = {specNames}")
-    if len(specNames) == 0:
-        specName = "spec.py"
-    else:
-        specName = specNames[0]["parameterValue"]["value"]
-        validate_filename(specName)
-    specNames = [modelParameter for modelParameter in modelParameters if modelParameter["id"] == "libraryPath"]
-    if len(specNames) == 0:
-        pythonLibrary = Nothing
-    else:
-        pythonLibrary = Just(specNames[0]["parameterValue"]["value"])
-        pythonLibrary.map(validate_filename)
-    nthreadsmps = [modelParameter for modelParameter in modelParameters if modelParameter["id"] == "nthreads"]
-    if len(nthreadsmps) == 0:
-        nthreads = 3
-    else:
-        nthreads = int(nthreadsmps[0]["parameterValue"]["value"])
+    modelParametersDefault = settingsDefault.get("modelParameters", [])
+    patientVariables = settingsRequested.get("patientVariables", settingsRequested.get("patientVariables", []))
+
+    specName = getModelParameter(modelParameters, "specName", identity, lambda: getModelParameter(modelParametersDefault, "specName", identity, lambda: "spec.py"))
+    logger.info(f"specName = {specName}")
+    validate_filename(specName)
     
-    levelmps = [modelParameter for modelParameter in modelParameters if modelParameter["id"] == "level"]
-    if len(levelmps) == 0:
-        level = 0
-    else:
-        level = int(levelmps[0]["parameterValue"]["value"])
+    pythonLibrary = getModelParameter(modelParameters, "libraryPath", Just, lambda: getModelParameter(modelParametersDefault, "libraryPath", Just, lambda: Nothing))
+    pythonLibrary.map(validate_filename)
+    
+    nthreads = getModelParameter(modelParameters, "nthreads", int, lambda: getModelParameter(modelParametersDefault, "nthreads", int, lambda: 3))
+    
+    level = getModelParameter(modelParameters, "level", int, lambda: getModelParameter(modelParametersDefault, "level", int, lambda: 0))
     
     spec_path = Path(__file__).parent.parent / "config" / specName
     lib_path = pythonLibrary.map(lambda pythonLibrary: str(Path(__file__).parent.parent / "config" / pythonLibrary)).rec(lambda x: [x], [])
@@ -80,20 +81,23 @@ def mappingClinicalFromData(body):
 
     logger.info(f"spec = {spec}")
 
-    yappi.start()
+    profile = os.environ.get("PROFILE")
+    if profile is not None:
+        yappi.start()
 
     res = start_python(nthreads, py = spec, data = {
         "patientIds": body["patientIds"],
         "patientVariables": patientVariables,
         "timestamp": body["timestamp"],
         "data": body["data"]
-    }, output_path = None, system_paths = lib_path, validate_spec = False, level=level)
+    }, output_path = None, system_paths = lib_path, validate_spec = False, level=level, object_store=None)
 
-    yappi.stop()
+    if profile is not None:
+        yappi.stop()
 
-    stats = yappi.get_func_stats()
-    stats.sort("tsub")
-    stats.print_all()
+        stats = yappi.get_func_stats()
+        stats.sort("tsub")
+        stats.print_all()
 
     ret = None
     for k,v in res.items():
@@ -103,33 +107,17 @@ def mappingClinicalFromData(body):
     return json.loads(json.dumps(ret))
         
 
-config = {
-    "title": "smarthealthit.org tx-parallex variable mapper",
-    "pluginType": "m",
-    "pluginTypeTitle": "Mapping",
-    "pluginSelectors": [],
-    "supportedPatientVariables": [
-        {
-            "id": i,
-            "title": t,
-            "legalValues": lv
-        } for i,t,lv in [
-            ("LOINC:2160-0", "Serum creatinine", {"type": "number"}),
-            ("LOINC:82810-3", "Pregnancy", {"type": "boolean"}),
-            ("HP:0001892", "Bleeding", {"type": "boolean"}),
-            ("HP:0000077", "Kidney dysfunction", {"type": "boolean"}),
-            ("LOINC:30525-0", "Age", {"type": "integer"}),
-            ("LOINC:54134-2", "Race", {"type": "string"}),
-            ("LOINC:54120-1", "Ethnicity", {"type": "string"}),
-            ("LOINC:21840-4", "Sex", {"type": "string"}),
-            ("LOINC:8302-2", "Height", {"type": "number"}),
-            ("LOINC:29463-7", "Weight", {"type": "number"}),
-            ("LOINC:39156-5", "BMI", {"type": "number"})
-        ]
-    ]
-}
-
-
 def get_config():
-    return config
+    settingsDefault = None if config_url is None else requests.get(config_url).json().get("settingsDefaults", None)
+
+    return {
+        "title": "pds parallex mapper",
+        'piid': "pdspi-mapper-parallex-example",
+        "pluginType": "m",
+        **({
+            "settingsDefaults": settingsDefault,
+        } if settingsDefault is not None else {}),
+        "pluginTypeTitle": "Mapping"
+    }
+
 
